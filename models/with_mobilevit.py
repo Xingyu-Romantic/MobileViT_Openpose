3ee806fd904efc1788519017a38611a826916cb7
Xingyu-Romantic <a940858626@qq.com>
2021-10-31 17:01:53 +0800

    Update MobileViT logical

--- models/with_mobilevit.py
+++ models/with_mobilevit.py
@@ -137,7 +137,7 @@ class Attention(nn.Layer):
         qkv=self.to_qkv(x).chunk(3,axis=-1)
         q,k,v=map(lambda t:rearrange(t.numpy(),'b p n (h d) -> b p h n d',h=self.heads),qkv)
         q, k, v = paddle.to_tensor(q), paddle.to_tensor(k), paddle.to_tensor(v)
-        dots=paddle.matmul(q,k.transpose((-1,-2)))*self.scale
+        dots=paddle.matmul(q,k.transpose((0, 1, 3, 2)))*self.scale
         attn=self.attend(dots)
         out=paddle.matmul(attn,v)
         #out = out.transpose((0, 2, 1, 3)).reshape((B_, N, C))
